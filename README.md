673fff8ecf41d1d8cbc03aa0e0bc2e45b948622b
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2020-09-07 00:42:58 +0100

    First commit - mirror copy of BotcoinATMs

---
+++ README.md
@@ -0,0 +1,40 @@
+# Assignment 2 - Enterprise Web Development
+
+## Description
+
+BitcoinsATMsV2 is an expanded in it's functionality adition to previous [repository](https://github.com/c4rt0/BitcoinATMs) of same title (off course without the V2 suffix :D ). The reason for a new repo is the fact that we all learn, and since in my opinion branching on previous version of this API was outrageously inconsistent... here I'm trying to improve on my weak skills.
+This (first) commit is a mirror copy of previous repo, improved only by the quality (as you can tell) of readme.md file.
+
+
+## Installation
+
+1. To install all necessary dependencies run  
+
+```bash
+npm install
+```
+
+## Usage
+
+
+1. Make sure you are in project directory where index.js is located. Confirm existence of "db" directory - if such folder does'nt exist, change that.
+
+2. Start mongodb with:
+
+```bash
+mongod -dbpath db
+```
+
+(This will fail if you skipped step one and db folder is not yet present in your working directory)
+
+Instructions regarding this aplication assume you are using WebStorm as your IDE.
+Navigate to location where routes.js is located and run index.js (mongodb should be already up and running)
+
+
+## Login
+
+As testing credentials use :
+
+user : homer@simpson.com
+
+password : secret
---
+++ app/controllers/atms.js
@@ -0,0 +1,143 @@
+'use strict';
+
+const Addition = require('../models/addition');
+const User = require('../models/user');
+const Candidate = require('../models/atm');
+const Joi = require('@hapi/joi');
+
+
+const Atms = {
+  home: {
+    handler: async function (request, h) {
+      return h.view('home', { title: 'Make a Addition' });
+    }
+  },
+  list: {
+    handler: async function (request, h) {
+      try {
+        const additions = await Addition.find().populate('donor').lean();
+        return h.view('list', {
+          title: 'Atms to Date',
+          additions: additions
+        });
+      } catch (err) {
+        return h.view('main', { errors: [{ message: err.message }] });
+      }
+    }
+  },
+  addAtm: {
+    handler: async function (request, h) {
+      try {
+
+        //const id = request.auth.credentials.id;
+        //const user = await User.findById(id);
+
+
+        const data = request.payload;
+
+        if (data.id) {
+
+
+          var atm = await Addition.findById(data.id);
+          //console.log(atm);
+          atm.name = data.name;
+          atm.category = data.category;
+          atm.description = data.description;
+
+          await atm.save();
+
+        }
+        else {
+
+          const newAddition = new Addition({
+            name: data.name,
+            category: data.category,
+            description: data.description,
+            /*        candidate: candidate._id */
+          });
+          await newAddition.save();
+
+        }
+
+        return h.redirect('/list');
+      } catch (err) {
+        return h.view('main', { errors: [{ message: err.message }] });
+      }
+    }
+  },
+
+  atmAdit: {
+    handler: async function (request, h) {
+      var id = request.params.id
+
+
+      var atm = {};
+
+      const additions = await Addition.find().populate('donor').lean();
+
+      additions.forEach(element => {
+        if (element._id == id) {
+          atm = element;
+          return true
+        }
+      });
+      return h.view('atm_setting', { atm: atm });
+    }
+  },
+  /*
+    validate: {
+      payload: {
+        name: Joi.string().required(),
+        category: Joi.string().required(),
+        description: Joi.string().required()
+      },
+      options: {
+        abortEarly: false
+      },
+      failAction: function (request, h, error) {
+        return h
+          .view('atm_Settings', {
+            title: 'Sign up error',
+            errors: error.details
+          })
+          .takeover()
+          .code(400);
+      }
+    },
+    */
+  add_adit_Form: {
+    handler: async function (request, h) {
+      try {
+
+        var id = request.params.id
+        console.log(id);
+        /*
+        const userEdit = request.payload;
+        const id = request.auth.credentials.id;
+        const user = await Addition.findById(id);
+        user.name = userEdit.name;
+        user.category = userEdit.category;
+        user.description = userEdit.description;
+        await user.save();
+        */
+        return h.redirect('/list');
+      } catch (err) {
+        return h.view('main', { errors: [{ message: err.message }] });
+      }
+    }
+  },
+  atmDelete: {
+    handler: async function (request, h) {
+      try {
+        var id = request.params.id;
+        var atm = await Addition.findById(id);
+        atm.remove();
+        return h.redirect('/list');
+      } catch (err) {
+        return h.view('main', { errors: [{ message: err.message }] });
+      }
+    }
+  }
+};
+
+module.exports = Atms;
