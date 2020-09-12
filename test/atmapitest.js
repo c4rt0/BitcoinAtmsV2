a6686feaeffe0bec86819fe8e2f96f756ff63aa5
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2020-09-12 13:04:01 +0100

    Implementing a class into tests - improving tdd

--- test/atmapitest.js
+++ test/atmapitest.js
@@ -1,55 +1,78 @@
 'use strict';
 
 const assert = require('chai').assert;
-const axios = require('axios');
+const AtmService = require('./atmservice');
+const fixtures = require('./fixtures.json');
 
-suite('ATM API tests', function () {
+suite('Atm API tests', function () {
 
-  test('get atmapi', async function () {
-    const response = await axios.get('http://localhost:3000/api/atmapi');
-    console.log(response.data);
-    const atmapi = response.data;
-    //assert.equal(12, atmapi.length);
+  let atm = fixtures.atms;
+  let newAtm = fixtures.newAtm;
 
-    assert.equal(atmapi[0].name, 'Enniscorthy BTC 1');
-    assert.equal(atmapi[0].category, 'Bitcoin only');
-    assert.equal(atmapi[0].description, 'Yellow');
+  const atmService = new AtmService('http://localhost:3000');
 
-    assert.equal(atmapi[1].name, 'Enniscorthy BTC 2');
-    assert.equal(atmapi[1].category, 'Multiple cryptocurrencies');
-    assert.equal(atmapi[1].description, 'Green');
-
-    assert.equal(atmapi[2].name, 'Enniscorthy BTC 3');
-    assert.equal(atmapi[2].category, 'Bitcoin only');
-    assert.equal(atmapi[2].description, 'White');
-  });
-  test('get one atm', async function () {
-    let response = await axios.get('http://localhost:3000/api/atmapi');
-    const atmapi = response.data;
-    //assert.equal(12, atmapi.length);
-
-    const oneAtmUrl = 'http://localhost:3000/api/atmapi/' + atmapi[0]._id;
-    response = await axios.get(oneAtmUrl);
-    const oneAtmz = response.data;
-
-    assert.equal(oneAtmz.name, 'Enniscorthy BTC 1');
-    assert.equal(oneAtmz.category, 'Bitcoin only');
-    assert.equal(oneAtmz.description, 'Yellow');
-  });
   test('create an atm', async function () {
-    const atmUrl = 'http://localhost:3000/api/atmapi';
-    const newAtm = {
-      name: 'Wexford Btc 1',
-      category: 'Bitcoin only',
-      description: 'Black',
-    };
-
-    const response = await axios.post(atmUrl, newAtm);
-    const returnedAtm = response.data;
-    assert.equal(201, response.status);
-
-    assert.equal(returnedAtm.name, 'Wexford Btc 1');
-    assert.equal(returnedAtm.category, 'Bitcoin only');
-    assert.equal(returnedAtm.description, 'Black');
+    const returnedAtm = await atmService.createAtm(newAtm);
+    assert.equal(returnedAtm.name, newAtm.name);
+    assert.equal(returnedAtm.category, newAtm.category);
+    assert.equal(returnedAtm.description, newAtm.description);
+    assert.isDefined(returnedAtm._id);
   });
 });
+
+
+// 'use strict';
+//
+// const assert = require('chai').assert;
+// const axios = require('axios');
+//
+// suite('ATM API tests', function () {
+//
+//   test('get atmapi', async function () {
+//     const response = await axios.get('http://localhost:3000/api/atmapi');
+//     console.log(response.data);
+//     const atmapi = response.data;
+//     //assert.equal(12, atmapi.length);
+//
+//     assert.equal(atmapi[0].name, 'Enniscorthy BTC 1');
+//     assert.equal(atmapi[0].category, 'Bitcoin only');
+//     assert.equal(atmapi[0].description, 'Yellow');
+//
+//     assert.equal(atmapi[1].name, 'Enniscorthy BTC 2');
+//     assert.equal(atmapi[1].category, 'Multiple cryptocurrencies');
+//     assert.equal(atmapi[1].description, 'Green');
+//
+//     assert.equal(atmapi[2].name, 'Enniscorthy BTC 3');
+//     assert.equal(atmapi[2].category, 'Bitcoin only');
+//     assert.equal(atmapi[2].description, 'White');
+//   });
+//   test('get one atm', async function () {
+//     let response = await axios.get('http://localhost:3000/api/atmapi');
+//     const atmapi = response.data;
+//     //assert.equal(12, atmapi.length);
+//
+//     const oneAtmUrl = 'http://localhost:3000/api/atmapi/' + atmapi[0]._id;
+//     response = await axios.get(oneAtmUrl);
+//     const oneAtmz = response.data;
+//
+//     assert.equal(oneAtmz.name, 'Enniscorthy BTC 1');
+//     assert.equal(oneAtmz.category, 'Bitcoin only');
+//     assert.equal(oneAtmz.description, 'Yellow');
+//   });
+//   test('create an atm', async function () {
+//     const atmUrl = 'http://localhost:3000/api/atmapi';
+//     const newAtm = {
+//       name: 'Wexford Btc 1',
+//       category: 'Bitcoin only',
+//       description: 'Black',
+//     };
+//
+//     const response = await axios.post(atmUrl, newAtm);
+//     const returnedAtm = response.data;
+//     assert.equal(201, response.status);
+//
+//     assert.equal(returnedAtm.name, 'Wexford Btc 1');
+//     assert.equal(returnedAtm.category, 'Bitcoin only');
+//     assert.equal(returnedAtm.description, 'Black');
+//   });
+// });
---
+++ test/atmservice.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const axios = require('axios');
+const baseUrl = 'http://localhost:3000';
+
+class AtmService {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  async getAtms() {
+    const response = await axios.get(this.baseUrl + '/api/atmapi');
+    return response.data;
+  }
+
+  async getAtm(id) {
+    try {
+      const response = await axios.get(this.baseUrl + '/api/atmapi/' + id);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async createAtm(newAtm) {
+    const response = await axios.post(this.baseUrl + '/api/atmapi', newAtm);
+    return response.data;
+  }
+
+  async deleteAllAtms() {
+    const response = await axios.delete(this.baseUrl + '/api/atmapi');
+    return response.data;
+  }
+
+  async deleteOneAtm(id) {
+    const response = await axios.delete(this.baseUrl + '/api/atmapi/' + id);
+    return response.data;
+  }
+
+  async getUsers() {
+    const response = await axios.get(this.baseUrl + '/api/users');
+    return response.data;
+  }
+
+  async getUser(id) {
+    try {
+      const response = await axios.get(this.baseUrl + '/api/user/' + id);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async createUser(newUser) {
+    const response = await axios.post(this.baseUrl + '/api/users', newUser);
+    return response.data;
+  }
+}
+
+module.exports = AtmService;
