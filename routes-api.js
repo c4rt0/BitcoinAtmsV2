4ca231a4eaf252f5180fbf85fbc7650cea41d926
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2020-09-12 22:14:05 +0100

    TDD improved - 7 tests passing

--- routes-api.js
+++ routes-api.js
@@ -4,4 +4,6 @@ module.exports = [
   { method: 'GET', path: '/api/atmapi', config: atmapi.find },
   { method: 'GET', path: '/api/atmapi/{id}', config: atmapi.findOne },
   { method: 'POST', path: '/api/atmapi', config: atmapi.create },
+  { method: 'DELETE', path: '/api/atmapi/{id}', config: atmapi.deleteOne },
+  { method: 'DELETE', path: '/api/atmapi', config: atmapi.deleteAll },
 ];
--- test/atmapitest.js
+++ test/atmapitest.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const assert = require('chai').assert;
-const AtmService = require('./atmservice');
+const AtmService = require('./atm-service');
 const fixtures = require('./fixtures.json');
 const _ = require('lodash');
+
 
 suite('Atm API tests', function () {
 
@@ -12,14 +13,67 @@ suite('Atm API tests', function () {
 
   const atmService = new AtmService('http://localhost:3000');
 
-  //  V3
-  test('Create an atm', async function () {
+  setup(async function () {
+    await atmService.deleteAllAtms();
+  });
+
+  teardown(async function () {
+    await atmService.deleteAllAtms();
+  });
+
+  test('Create an Atm', async function () {
     const returnedAtm = await atmService.createAtm(newAtm);
-    assert(_.some([returnedAtm], newAtm),  'returnedAtm must be a superset of newAtm');
+    assert(_.some([returnedAtm], newAtm), 'returnedAtm must be a superset of newAtm');
     assert.isDefined(returnedAtm._id);
   });
-});
 
+  test('Get atm', async function () {
+    const c1 = await atmService.createAtm(newAtm);
+    const c2 = await atmService.getAtm(c1._id);
+    assert.deepEqual(c1, c2);
+  });
+
+  test('Get invalid atm', async function () {
+    const c1 = await atmService.getAtm('1234');
+    assert.isNull(c1);
+    const c2 = await atmService.getAtm('012345678901234567890123');
+    assert.isNull(c2);
+  });
+
+  test('Get all atms', async function () {
+    for (let c of atm) {
+      await atmService.createAtm(c);
+    }
+
+    const allAtms = await atmService.getAtms();
+    assert.equal(allAtms.length, atm.length);
+  });
+
+  test('Get Atm detail', async function () {
+    for (let c of atm) {
+      await atmService.createAtm(c);
+    }
+
+    const allAtms = await atmService.getAtms();
+    for (var i = 0; i < atm.length; i++) {
+      assert(_.some([allAtms[i]], atm[i]), 'returnedAtm must be a superset of newAtm');
+    }
+  });
+
+  test('Get all Atms empty', async function () {
+    const allAtms = await atmService.getAtms();
+    assert.equal(allAtms.length, 0);
+  });
+
+  test('Delete an Atm', async function () {
+    let c = await atmService.createAtm(newAtm);
+    assert(c._id != null);
+    await atmService.deleteOneAtm(c._id);
+    c = await atmService.getAtm(c._id);
+    assert(c == null);
+  });
+
+});
   //  V2
   // test('create a candidate', async function () {
   //   const returnedAtm = await atmService.createAtm(newAtm);
