15569cccac1ab3e25c9759b2508acd93305cb05a
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2020-09-12 22:30:22 +0100

    Added more tests - total of 14

--- routes-api.js
+++ routes-api.js
@@ -1,4 +1,5 @@
 const atmapi = require('./app/api/atmapi');
+const Users= require('./app/api/users');
 
 module.exports = [
   { method: 'GET', path: '/api/atmapi', config: atmapi.find },
@@ -6,4 +7,9 @@ module.exports = [
   { method: 'POST', path: '/api/atmapi', config: atmapi.create },
   { method: 'DELETE', path: '/api/atmapi/{id}', config: atmapi.deleteOne },
   { method: 'DELETE', path: '/api/atmapi', config: atmapi.deleteAll },
+  { method: 'GET', path: '/api/users', config: Users.find },
+  { method: 'GET', path: '/api/users/{id}', config: Users.findOne },
+  { method: 'POST', path: '/api/users', config: Users.create },
+  { method: 'DELETE', path: '/api/users/{id}', config: Users.deleteOne },
+  { method: 'DELETE', path: '/api/users', config: Users.deleteAll }
 ];
